a3c0e8b2811486578fc5b44337c006e2970f7829
Messias Soares <soaresmessiasg130@gmail.com>
2020-07-27 20:51:32 -0300

    Comment Security Dependencies

--- src/main/java/com/codeblog/codeblog/Mock/MockPost.java
+++ src/main/java/com/codeblog/codeblog/Mock/MockPost.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
+// import javax.annotation.PostConstruct;
 
 @Component
 public class MockPost {
