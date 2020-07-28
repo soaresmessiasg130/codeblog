fd3d7b8f5823a400419d43c9b5fb2d37447b6fe3
Messias Soares <soaresmessiasg130@gmail.com>
2020-07-28 20:55:11 -0300

    Post Detail Method

--- src/main/java/com/codeblog/codeblog/Controller/CodeblogController.java
+++ src/main/java/com/codeblog/codeblog/Controller/CodeblogController.java
@@ -7,6 +7,7 @@ import com.codeblog.codeblog.Service.ICodeblogService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,6 +25,17 @@ public class CodeblogController {
     List<Post> posts = _service.findAll();
 
     view.addObject("AllPosts", posts);
+
+    return view;
+  }
+
+  @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+  public ModelAndView GetPostDetails (@PathVariable("id") long id) {
+    ModelAndView view = new ModelAndView("post-details");
+
+    Post post = _service.findById(id);
+
+    view.addObject("Post", post);
 
     return view;
   }
